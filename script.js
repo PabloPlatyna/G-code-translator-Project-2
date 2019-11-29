61a49b19df27e68b312460b3aa40f003935f5f55
Paweł Pyc <pawelpyc154@gmail.com>
2019-11-29 17:09:29 +0100

    work

--- script.js
+++ script.js
@@ -9,22 +9,25 @@ const output = document.querySelector(".output>textarea");
 
 // Database
 let sinumerik = {
-  1: "g00",
-  2: "g01",
-  3: "g02",
-  4: "g03"
+    1: "g00",
+    2: "g01",
+    3: "g02",
+    4: "g03",
+
 };
 let hass = {
-  1: "g0",
-  2: "g1",
-  3: "g2",
-  4: "g3"
+    1: "g0",
+    2: "g1",
+    3: "g2",
+    4: "g3",
+
 };
 let hh = {
-  1: "g0000",
-  2: "g0001",
-  3: "g0002",
-  4: "g0003"
+    1: "g0000",
+    2: "g0001",
+    3: "g0002",
+    4: "g0003",
+
 };
 
 //choice language
@@ -34,22 +37,21 @@ let LnOutPut = hass;
 getText.addEventListener("keyup", getText1, false);
 
 function getText1() {
-  let input = this.value;
-  //replace forEach Database
-  Object.keys(LnInput).forEach(function(key) {
-    let regex = new RegExp(LnInput[key], "g");
-    input = input.replace(regex, LnOutPut[key]);
-    //replace forEach Database WITH toUpperCase
-    let regexUP = new RegExp(LnInput[key].toUpperCase(), "g");
-    input = input.replace(regexUP, LnOutPut[key].toUpperCase());
-  });
-  output.value = input;
-  // localStorage set input
-  output.style.height = getText.style.height;
-  localStorage.setItem("input", this.value);
+    let input = this.value;
+    //replace forEach Database
+    Object.keys(LnInput).forEach(function (key) {
+        let regex = new RegExp(LnInput[key], "g");
+        input = input.replace(regex, LnOutPut[key]);
+        //replace forEach Database WITH toUpperCase
+        let regexUP = new RegExp(LnInput[key].toUpperCase(), "g");
+        input = input.replace(regexUP, LnOutPut[key].toUpperCase());
+    });
+    output.value = input;
+    // localStorage set input
+    output.style.height = getText.style.height;
+    localStorage.setItem("input", this.value);
 }
 
-set;
 
 const choice = document.querySelectorAll(".choice");
 // choice.forEach((button) => {
@@ -79,179 +81,179 @@ const choiceOut = document.querySelectorAll(".choiceOut");
 // })
 
 function choise11(choiceOut, button) {
-  choiceOut.forEach(e => {
-    e.classList.remove("choicefocus");
-  });
-  button.classList.add("choicefocus");
-  return eval(button.getAttribute("data-value"));
+    choiceOut.forEach(e => {
+        e.classList.remove("choicefocus");
+    });
+    button.classList.add("choicefocus");
+    return eval(button.getAttribute("data-value"));
 }
 choice.forEach(button => {
-  button.addEventListener("click", () => {
-    let test5 = button.getAttribute("data-value").trim();
-    let test6 = document
-      .querySelector(".choiceOut.choicefocus ")
-      .getAttribute("data-value")
-      .trim();
+    button.addEventListener("click", () => {
+        let test5 = button.getAttribute("data-value").trim();
+        let test6 = document
+            .querySelector(".choiceOut.choicefocus ")
+            .getAttribute("data-value")
+            .trim();
 
-    if (test5 == test6) {
-      let change = LnInput;
-      LnInput = LnOutPut;
-      LnOutPut = change;
-      // choice.forEach((e) => {
-      //     if (e.classList.contains("choicefocus")) {
-      //         var Sinput = (e.getAttribute("data-value")).trim();
+        if (test5 == test6) {
+            let change = LnInput;
+            LnInput = LnOutPut;
+            LnOutPut = change;
+            // choice.forEach((e) => {
+            //     if (e.classList.contains("choicefocus")) {
+            //         var Sinput = (e.getAttribute("data-value")).trim();
 
-      //         choice.forEach((e) => {
-      //             e.classList.remove("choicefocus")
-      //         })
+            //         choice.forEach((e) => {
+            //             e.classList.remove("choicefocus")
+            //         })
 
-      //     }
-      // })
+            //     }
+            // })
 
-      // choiceOut.forEach((e) => {
+            // choiceOut.forEach((e) => {
 
-      //     if (e.classList.contains("choicefocus")) {
-      //         var Soutput = (e.getAttribute("data-value"));
-      //         choiceOut.forEach((e) => {
-      //             e.classList.remove("choicefocus");
-      //         })
-      //     }
-      // })
+            //     if (e.classList.contains("choicefocus")) {
+            //         var Soutput = (e.getAttribute("data-value"));
+            //         choiceOut.forEach((e) => {
+            //             e.classList.remove("choicefocus");
+            //         })
+            //     }
+            // })
 
-      for (i = 0; i < choice.length; i++) {
-        if (choice[i].classList.contains("choicefocus")) {
-          var Sinput = choice[i].getAttribute("data-value").trim();
+            for (i = 0; i < choice.length; i++) {
+                if (choice[i].classList.contains("choicefocus")) {
+                    var Sinput = choice[i].getAttribute("data-value").trim();
 
-          choice.forEach(e => {
-            e.classList.remove("choicefocus");
-          });
+                    choice.forEach(e => {
+                        e.classList.remove("choicefocus");
+                    });
+                }
+            }
+            for (i = 0; i < choiceOut.length; i++) {
+                if (choiceOut[i].classList.contains("choicefocus")) {
+                    var Soutput = choiceOut[i].getAttribute("data-value").trim();
+
+                    choiceOut.forEach(e => {
+                        e.classList.remove("choicefocus");
+                    });
+                }
+            }
+
+            document.querySelectorAll(".choiceOut").forEach(e => {
+                let test1 = e.getAttribute("data-value").trim();
+                if (test1 == Sinput) {
+                    e.classList.add("choicefocus");
+                }
+            });
+            document.querySelectorAll(".choice").forEach(e => {
+                let test1 = e.getAttribute("data-value").trim();
+                if (test1 == Soutput) {
+                    e.classList.add("choicefocus");
+                }
+            });
+        } else {
+            LnInput = choise11(choice, button);
         }
-      }
-      for (i = 0; i < choiceOut.length; i++) {
-        if (choiceOut[i].classList.contains("choicefocus")) {
-          var Soutput = choiceOut[i].getAttribute("data-value").trim();
 
-          choiceOut.forEach(e => {
-            e.classList.remove("choicefocus");
-          });
-        }
-      }
+        LnInput = choise11(choice, button);
 
-      document.querySelectorAll(".choiceOut").forEach(e => {
-        let test1 = e.getAttribute("data-value").trim();
-        if (test1 == Sinput) {
-          e.classList.add("choicefocus");
-        }
-      });
-      document.querySelectorAll(".choice").forEach(e => {
-        let test1 = e.getAttribute("data-value").trim();
-        if (test1 == Soutput) {
-          e.classList.add("choicefocus");
-        }
-      });
-    } else {
-      LnInput = choise11(choice, button);
-    }
+        // if (LnInput == LnOutPut) {
+        //     console.log("elo");
+        // }
 
-    LnInput = choise11(choice, button);
-
-    // if (LnInput == LnOutPut) {
-    //     console.log("elo");
-    // }
-
-    const getText1 = document.querySelector(".input>textarea");
-    input = getText1.value;
-    //replace forEach Database
-    Object.keys(LnInput).forEach(function(key) {
-      let regex = new RegExp(LnInput[key], "g");
-      input = input.replace(regex, LnOutPut[key]);
-      //replace forEach Database WITH toUpperCase
-      let regexUP = new RegExp(LnInput[key].toUpperCase(), "g");
-      input = input.replace(regexUP, LnOutPut[key].toUpperCase());
+        const getText1 = document.querySelector(".input>textarea");
+        input = getText1.value;
+        //replace forEach Database
+        Object.keys(LnInput).forEach(function (key) {
+            let regex = new RegExp(LnInput[key], "g");
+            input = input.replace(regex, LnOutPut[key]);
+            //replace forEach Database WITH toUpperCase
+            let regexUP = new RegExp(LnInput[key].toUpperCase(), "g");
+            input = input.replace(regexUP, LnOutPut[key].toUpperCase());
+        });
+        output.value = input;
     });
-    output.value = input;
-  });
 });
 choiceOut.forEach(button => {
-  button.addEventListener("click", () => {
-    let test5 = button.getAttribute("data-value").trim();
-    let test6 = document
-      .querySelector(".choice.choicefocus ")
-      .getAttribute("data-value")
-      .trim();
+    button.addEventListener("click", () => {
+        let test5 = button.getAttribute("data-value").trim();
+        let test6 = document
+            .querySelector(".choice.choicefocus ")
+            .getAttribute("data-value")
+            .trim();
 
-    if (test5 == test6) {
-      let change = LnInput;
-      LnInput = LnOutPut;
-      LnOutPut = change;
-      // choice.forEach((e) => {
-      //     if (e.classList.contains("choicefocus")) {
-      //         var Sinput = (e.getAttribute("data-value")).trim();
+        if (test5 == test6) {
+            let change = LnInput;
+            LnInput = LnOutPut;
+            LnOutPut = change;
+            // choice.forEach((e) => {
+            //     if (e.classList.contains("choicefocus")) {
+            //         var Sinput = (e.getAttribute("data-value")).trim();
 
-      //         choice.forEach((e) => {
-      //             e.classList.remove("choicefocus")
-      //         })
+            //         choice.forEach((e) => {
+            //             e.classList.remove("choicefocus")
+            //         })
 
-      //     }
-      // })
+            //     }
+            // })
 
-      // choiceOut.forEach((e) => {
+            // choiceOut.forEach((e) => {
 
-      //     if (e.classList.contains("choicefocus")) {
-      //         var Soutput = (e.getAttribute("data-value"));
-      //         choiceOut.forEach((e) => {
-      //             e.classList.remove("choicefocus");
-      //         })
-      //     }
-      // })
+            //     if (e.classList.contains("choicefocus")) {
+            //         var Soutput = (e.getAttribute("data-value"));
+            //         choiceOut.forEach((e) => {
+            //             e.classList.remove("choicefocus");
+            //         })
+            //     }
+            // })
 
-      for (i = 0; i < choice.length; i++) {
-        if (choice[i].classList.contains("choicefocus")) {
-          var Sinput = choice[i].getAttribute("data-value").trim();
+            for (i = 0; i < choice.length; i++) {
+                if (choice[i].classList.contains("choicefocus")) {
+                    var Sinput = choice[i].getAttribute("data-value").trim();
 
-          choice.forEach(e => {
-            e.classList.remove("choicefocus");
-          });
+                    choice.forEach(e => {
+                        e.classList.remove("choicefocus");
+                    });
+                }
+            }
+            for (i = 0; i < choiceOut.length; i++) {
+                if (choiceOut[i].classList.contains("choicefocus")) {
+                    var Soutput = choiceOut[i].getAttribute("data-value").trim();
+
+                    choiceOut.forEach(e => {
+                        e.classList.remove("choicefocus");
+                    });
+                }
+            }
+
+            document.querySelectorAll(".choiceOut").forEach(e => {
+                let test1 = e.getAttribute("data-value").trim();
+                if (test1 == Sinput) {
+                    e.classList.add("choicefocus");
+                }
+            });
+            document.querySelectorAll(".choice").forEach(e => {
+                let test1 = e.getAttribute("data-value").trim();
+                if (test1 == Soutput) {
+                    e.classList.add("choicefocus");
+                }
+            });
+        } else {
+            LnOutPut = choise11(choiceOut, button);
         }
-      }
-      for (i = 0; i < choiceOut.length; i++) {
-        if (choiceOut[i].classList.contains("choicefocus")) {
-          var Soutput = choiceOut[i].getAttribute("data-value").trim();
 
-          choiceOut.forEach(e => {
-            e.classList.remove("choicefocus");
-          });
-        }
-      }
-
-      document.querySelectorAll(".choiceOut").forEach(e => {
-        let test1 = e.getAttribute("data-value").trim();
-        if (test1 == Sinput) {
-          e.classList.add("choicefocus");
-        }
-      });
-      document.querySelectorAll(".choice").forEach(e => {
-        let test1 = e.getAttribute("data-value").trim();
-        if (test1 == Soutput) {
-          e.classList.add("choicefocus");
-        }
-      });
-    } else {
-      LnOutPut = choise11(choiceOut, button);
-    }
-
-    const getText1 = document.querySelector(".input>textarea");
-    input = getText1.value;
-    //replace forEach Database
-    Object.keys(LnInput).forEach(function(key) {
-      let regex = new RegExp(LnInput[key], "g");
-      input = input.replace(regex, LnOutPut[key]);
-      //replace forEach Database WITH toUpperCase
-      let regexUP = new RegExp(LnInput[key].toUpperCase(), "g");
-      input = input.replace(regexUP, LnOutPut[key].toUpperCase());
+        const getText1 = document.querySelector(".input>textarea");
+        input = getText1.value;
+        //replace forEach Database
+        Object.keys(LnInput).forEach(function (key) {
+            let regex = new RegExp(LnInput[key], "g");
+            input = input.replace(regex, LnOutPut[key]);
+            //replace forEach Database WITH toUpperCase
+            let regexUP = new RegExp(LnInput[key].toUpperCase(), "g");
+            input = input.replace(regexUP, LnOutPut[key].toUpperCase());
+        });
+        output.value = input;
     });
-    output.value = input;
-  });
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -260,92 +262,92 @@ const exchange = document.querySelector(".exchange");
 exchange.addEventListener("click", exchange1, false);
 
 function exchange1() {
-  let change = LnInput;
-  LnInput = LnOutPut;
-  LnOutPut = change;
-  // choice.forEach((e) => {
-  //     if (e.classList.contains("choicefocus")) {
-  //         var Sinput = (e.getAttribute("data-value")).trim();
+    let change = LnInput;
+    LnInput = LnOutPut;
+    LnOutPut = change;
+    // choice.forEach((e) => {
+    //     if (e.classList.contains("choicefocus")) {
+    //         var Sinput = (e.getAttribute("data-value")).trim();
 
-  //         choice.forEach((e) => {
-  //             e.classList.remove("choicefocus")
-  //         })
+    //         choice.forEach((e) => {
+    //             e.classList.remove("choicefocus")
+    //         })
 
-  //     }
-  // })
+    //     }
+    // })
 
-  // choiceOut.forEach((e) => {
+    // choiceOut.forEach((e) => {
 
-  //     if (e.classList.contains("choicefocus")) {
-  //         var Soutput = (e.getAttribute("data-value"));
-  //         choiceOut.forEach((e) => {
-  //             e.classList.remove("choicefocus");
-  //         })
-  //     }
-  // })
+    //     if (e.classList.contains("choicefocus")) {
+    //         var Soutput = (e.getAttribute("data-value"));
+    //         choiceOut.forEach((e) => {
+    //             e.classList.remove("choicefocus");
+    //         })
+    //     }
+    // })
 
-  for (i = 0; i < choice.length; i++) {
-    if (choice[i].classList.contains("choicefocus")) {
-      var Sinput = choice[i].getAttribute("data-value").trim();
+    for (i = 0; i < choice.length; i++) {
+        if (choice[i].classList.contains("choicefocus")) {
+            var Sinput = choice[i].getAttribute("data-value").trim();
 
-      choice.forEach(e => {
-        e.classList.remove("choicefocus");
-      });
+            choice.forEach(e => {
+                e.classList.remove("choicefocus");
+            });
+        }
     }
-  }
-  for (i = 0; i < choiceOut.length; i++) {
-    if (choiceOut[i].classList.contains("choicefocus")) {
-      var Soutput = choiceOut[i].getAttribute("data-value").trim();
+    for (i = 0; i < choiceOut.length; i++) {
+        if (choiceOut[i].classList.contains("choicefocus")) {
+            var Soutput = choiceOut[i].getAttribute("data-value").trim();
 
-      choiceOut.forEach(e => {
-        e.classList.remove("choicefocus");
-      });
+            choiceOut.forEach(e => {
+                e.classList.remove("choicefocus");
+            });
+        }
     }
-  }
 
-  document.querySelectorAll(".choiceOut").forEach(e => {
-    let test1 = e.getAttribute("data-value").trim();
-    if (test1 == Sinput) {
-      e.classList.add("choicefocus");
-    }
-  });
-  document.querySelectorAll(".choice").forEach(e => {
-    let test1 = e.getAttribute("data-value").trim();
-    if (test1 == Soutput) {
-      e.classList.add("choicefocus");
-    }
-  });
+    document.querySelectorAll(".choiceOut").forEach(e => {
+        let test1 = e.getAttribute("data-value").trim();
+        if (test1 == Sinput) {
+            e.classList.add("choicefocus");
+        }
+    });
+    document.querySelectorAll(".choice").forEach(e => {
+        let test1 = e.getAttribute("data-value").trim();
+        if (test1 == Soutput) {
+            e.classList.add("choicefocus");
+        }
+    });
 
-  document.querySelector(".input>textarea").value = document.querySelector(
-    ".output>textarea"
-  ).value;
+    document.querySelector(".input>textarea").value = document.querySelector(
+        ".output>textarea"
+    ).value;
 
-  const getText1 = document.querySelector(".input>textarea");
-  input = getText1.value;
-  //replace forEach Database
-  Object.keys(LnInput).forEach(function(key) {
-    let regex = new RegExp(LnInput[key], "g");
-    input = input.replace(regex, LnOutPut[key]);
-    //replace forEach Database WITH toUpperCase
-    let regexUP = new RegExp(LnInput[key].toUpperCase(), "g");
-    input = input.replace(regexUP, LnOutPut[key].toUpperCase());
-  });
-  output.value = input;
+    const getText1 = document.querySelector(".input>textarea");
+    input = getText1.value;
+    //replace forEach Database
+    Object.keys(LnInput).forEach(function (key) {
+        let regex = new RegExp(LnInput[key], "g");
+        input = input.replace(regex, LnOutPut[key]);
+        //replace forEach Database WITH toUpperCase
+        let regexUP = new RegExp(LnInput[key].toUpperCase(), "g");
+        input = input.replace(regexUP, LnOutPut[key].toUpperCase());
+    });
+    output.value = input;
 }
 
 if (localStorage.length) {
-  getText.value = localStorage.getItem("input");
-  getText1 = document.querySelector(".input>textarea");
-  input = getText1.value;
-  //replace forEach Database
-  Object.keys(LnInput).forEach(function(key) {
-    let regex = new RegExp(LnInput[key], "g");
-    input = input.replace(regex, LnOutPut[key]);
-    //replace forEach Database WITH toUpperCase
-    let regexUP = new RegExp(LnInput[key].toUpperCase(), "g");
-    input = input.replace(regexUP, LnOutPut[key].toUpperCase());
-  });
-  output.value = input;
+    getText.value = localStorage.getItem("input");
+    getText1 = document.querySelector(".input>textarea");
+    input = getText1.value;
+    //replace forEach Database
+    Object.keys(LnInput).forEach(function (key) {
+        let regex = new RegExp(LnInput[key], "g");
+        input = input.replace(regex, LnOutPut[key]);
+        //replace forEach Database WITH toUpperCase
+        let regexUP = new RegExp(LnInput[key].toUpperCase(), "g");
+        input = input.replace(regexUP, LnOutPut[key].toUpperCase());
+    });
+    output.value = input;
 } else {
-  getText.value = "";
+    getText.value = "";
 }
